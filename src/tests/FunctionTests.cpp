0d62226f63b96777a13925384fe33e3c746c7055
Jonathan Johnson <jon@nilobject.com>
2015-06-06 14:13:52 -0700

    [Delivers #96350650] Added more tests for functions.

--- src/tests/FunctionTests.cpp
+++ src/tests/FunctionTests.cpp
@@ -8,11 +8,29 @@
 using namespace std;
 using namespace Aise;
 
-TEST_CASE("Simple Function - static result", "[functions][do]") {
+TEST_CASE("Simple Function - static result", "[functions][do]") {\
 	auto env = new Aise::Environment();
 	Aise::Result result = env->Evaluate("(do (function return-5 () 5) (return-5))");
 	REQUIRE(result.Error() == false);
 	auto value = dynamic_pointer_cast<Integer>(result.Value());
 	REQUIRE(value);
 	REQUIRE(value->Value() == 5);
+}
+
+TEST_CASE("Simple Function - accept one parameter", "[functions][do]") {
+	auto env = new Aise::Environment();
+	Aise::Result result = env->Evaluate("(do (function multiply-by-5 (x) (multiply x 5)) (multiply-by-5 4))");
+	REQUIRE(result.Error() == false);
+	auto value = dynamic_pointer_cast<Integer>(result.Value());
+	REQUIRE(value);
+	REQUIRE(value->Value() == 20);
+}
+
+TEST_CASE("Simple Function - operate on two parameters", "[functions][do]") {
+	auto env = new Aise::Environment();
+	Aise::Result result = env->Evaluate("(do (function average (x y) (divide (add x y) 2)) (average 2 4))");
+	REQUIRE(result.Error() == false);
+	auto value = dynamic_pointer_cast<Integer>(result.Value());
+	REQUIRE(value);
+	REQUIRE(value->Value() == 3);
 }
